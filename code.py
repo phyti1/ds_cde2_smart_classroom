c92df2cd818e2f099bc66d7d1d109f983f98dbf9
Raphi0910 <raphael.strebel@hotmail.com>
2021-03-09 13:45:42 +0100

    Update code.py
    add alarm if the co2 is higher than 6000

--- code.py
+++ code.py
@@ -6,11 +6,21 @@ import busio
 from grove_ultrasonic_ranger import GroveUltrasonicRanger
 import display
 import pir
+import tm1637lib
+import pulseio
 
 from secrets import secrets
 
 # connect to wifi
 import wifi
+
+# buzzer
+cycle = 65535 // 5 # 20% power
+buzzer = pulseio.PWMOut(board.A0,
+  duty_cycle=cycle, variable_frequency=True)
+
+# 100000 is not hearable
+buzzer.frequency = 100000
 
 # CO2
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -27,6 +37,16 @@ while True:
         #display.show(round(sonar_distance))
         #time.sleep(1)
         display.show(round(scd.CO2))
+
+
+        if round(scd.CO2) > 6000:
+            buzzer.frequency = 880
+            time.sleep(0.3)
+            buzzer.frequency = 784
+            time.sleep(0.7)
+            buzzer.frequency = 100000
+
+
         is_pir_active = pir.measure()
         #display.show(str(is_pir_active))
         try:

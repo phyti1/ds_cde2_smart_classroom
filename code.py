cf915eb5939224b0206067c2600b04a9a0a0321f
Jelle Schutter <jelle@schutter.xyz>
2021-03-20 20:10:28 +0100

    disable status led

--- code.py
+++ code.py
@@ -1,7 +1,11 @@
+import neopixel
+import board
 from window_meter import WindowMeter
 
+# disable status led
+led = neopixel.NeoPixel(board.NEOPIXEL, 1)
+led.brightness = 0.0
 
 # Program entry-point
-
 main = WindowMeter(is_debugging=True)
 main.run()

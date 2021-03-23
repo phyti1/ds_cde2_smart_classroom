45ea9375f846f26de6c813f28fafa0bcafac5bc3
Jelle Schutter <jelle@schutter.xyz>
2021-03-23 21:48:59 +0100

    update secrets example

--- secrets.example.py
+++ secrets.example.py
@@ -1,6 +1,8 @@
 secrets = {
+    'uuid': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', # Get a UUID here: https://ddg.co/?q=guid
     'device': 'demo-device',
     'ssid': 'Einhornaufzuchtstation',
     'password': 'raphi123',
-    'endpoint': 'http://192.168.43.189/iot'
+    'endpoint': 'https://glusfqycvwrucp9-db202012181437.adb.eu-zurich-1.oraclecloudapps.com/ords/sensor_datalake/sens',
+    'oracle_token': '<INSERT TOKEN HERE>'
 }
